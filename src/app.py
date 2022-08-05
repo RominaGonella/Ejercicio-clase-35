4cc3b5cb25db7230df9639b786b63c019e3b86a0
RominaGonella <rominagonfur@gmail.com>
2022-08-05 23:26:02 +0000

    creamos nuestro primer archivo flask

--- src/app.py
+++ src/app.py
@@ -1,4 +1,11 @@
-from utils import db_connect
-engine = db_connect()
+from flask import Flask
 
-# your code here
+# creamos objeto flask
+app = Flask(__name__) # __name__ es alias de nombre del archivo
+
+@app.route('/') # va a ser una API web, necesita una ruta
+def hello_flask():
+    return 'Hello Flask'
+
+if __name__ == '__main__':
+    app.run(debug = True, host = '127.0.0.1', port = 5000)
